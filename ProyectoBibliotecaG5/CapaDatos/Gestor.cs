40391d2d6614bffc0a0e85b7f4065951144bcd82
Arkaitz Calvo Saldias <acalvosald@educacion.navarra.es>
2023-10-24 11:55:57 +0200

    Agregadas listas privadas

---
+++ ProyectoBibliotecaG5/CapaDatos/Gestor.cs
@@ -0,0 +1,19 @@
+﻿using CapaEntidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class Gestor
+    {
+        private Biblioteca biblioteca;
+        private List<Autor> autores;
+        private List<Libro> libros;
+        private List<Lector> lectores;
+        private List<Categoria> categorias;
+
+    }
+}
